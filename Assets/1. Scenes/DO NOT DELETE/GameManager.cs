4b97c58c361f4faf992c19e7d8a10993e0ee4cd0
JiaChau <jiacgsmst@gmail.com>
2024-06-30 20:55:40 -0400

    updated ingame scene with plants

---
+++ Assets/1. Scenes/DO NOT DELETE/GameManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public int coins = 6;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddCoins(int amount)
+    {
+        coins += amount;
+        UIManager.instance.UpdateCoinDisplay(coins);
+    }
+
+    public bool SpendCoins(int amount)
+    {
+        if (coins >= amount)
+        {
+            coins -= amount;
+            UIManager.instance.UpdateCoinDisplay(coins);
+            return true;
+        }
+        return false;
+    }
+}
